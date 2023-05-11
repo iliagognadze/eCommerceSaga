0a7efc03a689c72caf193dfa70d29788251d3f62
iliagognadze <paolokolau@gmail.com>
2023-05-11 21:53:01 +0400

    added entities and shared

---
+++ Shared/DTOs/OrderForCreationDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared.DTOs;
+
+public class OrderForCreationDto
+{
+    public int Id { get; set; }
+    public string CustomerName { get; set; }
+    public string CustomerEmail { get; set; }
+    public DateTime Date { get; set; }
+    public decimal TotalPrice { get; set; }
+    public List<OrderItem> Items { get; set; }
+}
---
+++ eCommerce.API/Controllers/OrdersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shared.DTOs;
+
+namespace eCommerce.API.Controllers;
+
+[ApiController]
+[Route("api/orders")]
+public class OrdersController : ControllerBase
+{
+    public OrdersController()
+    {
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateOrder([FromBody] OrderForCreationDto order)
+    {
+        return Ok(order);
+    }
+}
