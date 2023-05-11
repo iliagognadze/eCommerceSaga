0fb4fb22c48e1024c31bd4c53dd9e0ddab52e7c6
iliagognadze <paolokolau@gmail.com>
2023-05-11 21:57:52 +0400

    added models

---
+++ Entities/Models/OrderItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Models;
+
+public class OrderItem
+{
+    public int Id { get; set; }
+    public string ProductName { get; set; }
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+}
